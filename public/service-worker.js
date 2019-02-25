7f3b70e78b237f6bd798699b9488a9a2bd7f788d
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2019-02-25 17:22:28 -0600

    buttons added and autoplay video fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c518c399ec3d87469a688cf26ae26d8.js"
+  "/precache-manifest.33d0a06a2c48afc1122af46ab3621a31.js"
 );
 
 workbox.clientsClaim();
